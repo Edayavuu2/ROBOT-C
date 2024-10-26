5940614f28c498327535ddd9a0d6fcca9b5effb1
Edayavuu2 <w.edayavuz@gmail.com>
2024-10-26 18:42:27 +0300

    Add SAC algorithm and environment setup files

---
+++ pusher_env.py
@@ -0,0 +1,14 @@
+﻿import gym
+
+class PusherEnv(gym.Env):
+    def __init__(self):
+        # Pusher ortamını başlat
+        pass
+    
+    def step(self, action):
+        # Bir adım at
+        pass
+    
+    def reset(self):
+        # Ortamı sıfırla
+        pass
---
+++ train.py
@@ -0,0 +1,6 @@
+﻿from sac import SAC
+from pusher_env import PusherEnv
+
+env = PusherEnv()
+sac = SAC(env)
+sac.train(1000)  # 1000 bölüm eğitimi
